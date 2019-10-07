af5c2e0b8751e0915e57cb663178ab8e477bb7f2
Daniel Jones <daniel@jones.org.uk>
2019-10-07 14:06:57 +0100

    Ensure Synth is always freed

--- examples/hello-world.py
+++ examples/hello-world.py
@@ -22,9 +22,12 @@ server = Server()
 #-------------------------------------------------------------------------------
 # Create a Synth, set its parameter, query the parameter, and free it.
 #-------------------------------------------------------------------------------
-synth = Synth(server, "sine", { "freq" : 440.0, "gain" : -36.0 })
-print("Created synth")
-synth.get("freq", lambda n: print("Frequency: %.1f" % n))
-time.sleep(1.0)
-synth.free()
-print("Freed synth")
+try:
+    synth = Synth(server, "sine", { "freq" : 440.0, "gain" : -36.0 })
+    print("Created synth")
+    synth.get("freq", lambda n: print("Frequency: %.1f" % n))
+    while True:
+        time.sleep(0.1)
+except KeyboardInterrupt:
+    synth.free()
+    print("Freed synth")
