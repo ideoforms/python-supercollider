9b39de478cf68be74cb31d2584d1d1babc1be7bf
Daniel Jones <daniel@jones.org.uk>
2019-10-09 20:38:18 +0100

    Improve example comments

--- examples/buffer-set.py
+++ examples/buffer-set.py
@@ -1,16 +1,16 @@
 #!/usr/bin/env python3 
 
-#-------------------------------------------------------------------------------
-# Example of buffer creation and playback.
-#
-# Before running this script, the SC server must be started, and the following
-# SynthDef stored:
-#
-# SynthDef(\playbuf, { |out = 0, bufnum = 0, gain = 0.0|
-#     var data = PlayBuf.ar(1, bufnum, loop: 1) * gain.dbamp;
-#     Out.ar(out, Pan2.ar(data));
-# }).store;
-#-------------------------------------------------------------------------------
+"""
+Example: Buffer creation and playback.
+
+Before running this script, the SC server must be started, and the following
+SynthDef stored:
+
+SynthDef(\playbuf, { |out = 0, bufnum = 0, gain = 0.0|
+    var data = PlayBuf.ar(1, bufnum, loop: 1) * gain.dbamp;
+    Out.ar(out, Pan2.ar(data));
+}).store;
+"""
 
 from supercollider import Server, Synth, Buffer
 
@@ -33,7 +33,7 @@ print("Created buffer: %s" % buf.info)
 synth = Synth(server, 'playbuf', { "buffer" : buf, "gain" : -18.0 })
 try:
     while True:
-        buf.setn([ random.uniform(-1, 1) for n in range(buf.num_frames) ])
+        buf.set([ random.uniform(-1, 1) for n in range(buf.num_frames) ])
         time.sleep(0.1)
 except KeyboardInterrupt:
     synth.free()
--- examples/hello-world.py
+++ examples/hello-world.py
@@ -1,15 +1,15 @@
 #!/usr/bin/env python3
 
-#-------------------------------------------------------------------------------
-# Minimal example of using python-supercollider.
-#
-# Before running this script, the SC server must be started, and the following
-# SynthDef stored:
-#
-# SynthDef(\sine, { |out = 0, freq = 440.0, gain = 0.0|
-#     Out.ar(out, SinOsc.ar(freq) * gain.dbamp);
-# }).store;
-#-------------------------------------------------------------------------------
+"""
+Example: Create a sine wave synth.
+
+Before running this script, the SC server must be started, and the following
+SynthDef stored:
+
+SynthDef(\sine, { |out = 0, freq = 440.0, gain = 0.0|
+    Out.ar(out, SinOsc.ar(freq) * gain.dbamp);
+}).store;
+"""
 
 from supercollider import Server, Synth
 import time
