ba9852e1de0793c0579d02d0a04b67db27699d22
Daniel Jones <daniel@jones.org.uk>
2019-10-18 14:09:42 +0100

    Test both AudioBus and ControlBus

--- tests/test_bus.py
+++ tests/test_bus.py
@@ -5,15 +5,16 @@ from supercollider.globals  import ALLOCATOR_BUS_START_INDEX, ALLOCATOR_BUS_CAPA
 from tests.shared import server
 
 def test_bus(server):
-    bus1 = supercollider.ControlBus(server, 2)
-    assert bus1.id == ALLOCATOR_BUS_START_INDEX
-    bus2 = supercollider.ControlBus(server, 2)
-    assert bus2.id == ALLOCATOR_BUS_START_INDEX + 2
-    remaining_channel_count = ALLOCATOR_BUS_CAPACITY - 4
-    bus3 = supercollider.ControlBus(server, remaining_channel_count)
-    assert bus3.id == ALLOCATOR_BUS_START_INDEX + 4
-    with pytest.raises(supercollider.SuperColliderAllocationError) as excinfo:
-        bus4 = supercollider.ControlBus(server, 2)
-    bus1.free()
-    bus2.free()
-    bus3.free()
+    for cls in [ supercollider.AudioBus, supercollider.ControlBus ]:
+        bus1 = cls(server, 2)
+        assert bus1.id == ALLOCATOR_BUS_START_INDEX
+        bus2 = cls(server, 2)
+        assert bus2.id == ALLOCATOR_BUS_START_INDEX + 2
+        remaining_channel_count = ALLOCATOR_BUS_CAPACITY - 4
+        bus3 = cls(server, remaining_channel_count)
+        assert bus3.id == ALLOCATOR_BUS_START_INDEX + 4
+        with pytest.raises(supercollider.SuperColliderAllocationError) as excinfo:
+            bus4 = cls(server, 2)
+        bus1.free()
+        bus2.free()
+        bus3.free()
