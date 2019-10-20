ad392a9273935a371ac5f6c6d7d11ba8d9e86174
Daniel Jones <daniel@jones.org.uk>
2019-10-20 16:36:25 +0100

    Split out Synth get/set test

--- tests/test_synth.py
+++ tests/test_synth.py
@@ -6,6 +6,10 @@ from tests.shared import server
 def test_synth_create(server):
     synth = supercollider.Synth(server, "sine", { "freq": 440.0,  "gain": -24 })
     assert synth.id > 0
+    synth.free()
+
+def test_synth_get_set(server):
+    synth = supercollider.Synth(server, "sine", { "freq": 440.0,  "gain": -24 })
     assert synth.get("freq") == 440.0
     synth.set("freq", 880.0)
     assert synth.get("freq") == 880.0
